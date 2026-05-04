c20f8f717a27e47cacf9c6a78e2b154a4f8cdf68
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2026-05-04 18:16:32 -0300

    chore: roda lint no projeto

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
